119ff77b8313de8ee1493fd15ad82bb46244f718
lxnder <32546395+lxnder@users.noreply.github.com>
2020-06-29 16:07:15 +0200

    Apollo scaffolding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql/",
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <div></div>
+    </ApolloProvider>
+  );
+};
 
 export default App;
---
+++ src/lib/queries.js
@@ -0,0 +1,59 @@
+import { gql } from "apollo-boost";
+
+export const GET_LAUNCH = id => gql`
+  {
+    launch(id: ${id}) {
+      details
+      id
+      is_tentative
+      launch_date_unix
+      launch_site {
+        site_id
+        site_name
+        site_name_long
+      }
+      launch_success
+      launch_year
+      links {
+        presskit
+        flickr_images
+        article_link
+        video_link
+        wikipedia
+        mission_patch
+        mission_patch_small
+      }
+      mission_id
+      mission_name
+      rocket {
+        rocket {
+          name
+          type
+          description
+          id
+          wikipedia
+        }
+      }
+      ships {
+        name
+        type
+        id
+        image
+        url
+      }
+      upcoming
+    }
+  }
+`;
+
+export const GET_LAUNCHES = gql`
+  {
+    launches(sort: "mission_name", order: "asc") {
+      id
+      launch_date_unix
+      launch_success
+      mission_name
+      upcoming
+    }
+  }
+`;
